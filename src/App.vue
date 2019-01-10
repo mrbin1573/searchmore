93e34c3839954e290d6b38fcc4dc2794501e6336
MrBinUsername <1573406407@qq.com>
2019-01-10 17:49:35 +0800

    添加所有浏览器切换，修改获取百度提示的触发方式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="searchmore-content" class="" ref="onSearch">
+  <div id="searchmore-content" :class="searchResultShow == true ? 'on-search' : ''">
     <div class="searchmore-box">
       <!-- <div class="searchmore-logo-box">
         <span class="searchmore-name">杉木搜索</span>
@@ -9,23 +9,28 @@
       <div class="input-search-box">
         <!-- 搜索框 -->
         <div class="input-box">
-          <input type="text" id="inputObj" 
+          <input type="text" id="inputObj"
+          ref="inputbox"
           v-model="searchTxt"
           @keyup.enter="inputEnter"
-          @keyup="getBaidu"
+          @input="getBaidu"
           @blur="inputLeave"
           @focus="inputFocus"
           @keyup.up="tipUp"
           @keyup.down="tipDown"
           placeholder="请输入搜索内容" class="searchmore-input-text">
-          <label for="inputObj" class="searchmore-submit hover-bg" @click="searchNow">更多结果</label>
+          <div class="border-line line-top" :class="inputFocusBool == true ? 'focus' : ''"></div>
+          <div class="border-line line-bottom" :class="inputFocusBool == true ? 'focus' : ''"></div>
+
+          <div class="line-left" :class="inputFocusBool == true ? 'focus' : ''"></div>
+          <label class="searchmore-submit hover-bg" @click="searchNow">更多结果</label>
         </div>
 
         <!-- 搜索结果提示 -->
         <transition name="to-top">
-          <div class="search-tip" v-show="searchTipShow">
+          <div class="search-tip" v-show="searchTipObj.show">
             <ul>
-              <li v-for="tipItem in searchTipArr" @click="selectTip($event)"><span>{{tipItem}}</span></li>
+              <li class="tip-item" :class="{active:index == searchTipObj.nowIndex}" v-for="(tipItem, index) in searchTipObj.data" @click="selectTip(index)"><span>{{tipItem}}</span></li>
             </ul>
           </div>
         </transition>
@@ -33,7 +38,7 @@
         <!-- 设置页面 -->
         <div class="set-panel" @mouseover="showSetPanel" @mouseleave="hideSetPanel">
           
-          <!-- 设置图标 -->
+          <!-- 设置齿轮图标 -->
           <div class="set-icon">
             <i class="icon-setting"></i>
           </div>
@@ -54,11 +59,18 @@
 
               <!-- 浏览器列表 -->
               <div class="browser-list-box clearfix">
-                <div v-for="bsItem in browserArr" class="browser-item" @click="switchBrowser($event)">
-                  <div class="browser-icon" :style="bsItem.active == true ? bsItem.bgColor : ' '">
+                <div v-for="(bsItem, index) in browserArr" class="browser-item" @click="switchBrowser(index)">
+                  <div class="browser-icon" :style="bsItem.active == true ? bsItem.iconColorStyle : ' '">
                     <i :class="bsItem.icon"></i>
                   </div>
                   <h4 class="browser-name" :class="bsItem.active == true ? 'active' : ' '">{{bsItem.name}}</h4>
+                </div>
+                <!-- 手动添加按钮 -->
+                <div class="browser-item" @click="addBrowser()">
+                  <div class="browser-icon">
+                    <i class="icon-add"></i>
+                  </div>
+                  <h4 class="browser-name active">添加搜索</h4>
                 </div>
               </div>
 
@@ -97,14 +109,11 @@
         <div class="result-iframe-content">
           <transition-group name="to-bottom">
             <iframe class="iframe-item" 
-            ref="resultPanel"
             v-for="resultItemFrame in browserArr" 
-            v-if="resultItemFrame.icon != 'icon-add' && resultItemFrame.active == true" 
             v-show="resultItemFrame.current == true"
             :key="resultItemFrame.icon"
-            :style="resultItemFrame.name == '必应' ? 'margin-top: -35px' : ''"
-            :searchUrl = "resultItemFrame.browserUrl"
-            :current = "resultItemFrame.current"
+            :style="resultItemFrame.resultIframeStyle"
+            :src="resultItemFrame.resultSrc"
             frameborder="0"></iframe>
           </transition-group>
         </div>
@@ -124,10 +133,10 @@
         <transition-group name="to-bottom">
           <div class="item" 
           :key="bsItem.name"
-          :class="bsItem.use == true ? 'active hover-bg' : ''"
-          v-for="bsItem in browserArr" 
+          :class="bsItem.current == true ? 'active hover-bg' : ''"
+          v-for="(bsItem, index) in browserArr" 
           v-if="bsItem.icon != 'icon-add' && bsItem.active == true"
-          @click="changeBrowser($event)">
+          @click="changeBrowser(index)">
             <i :class="bsItem.icon"></i>
             {{bsItem.name}}
           </div>
@@ -138,8 +147,8 @@
 
     <!-- 背景 -->
     <div class="sm-bg" v-show="!searchResultShow">
-      <video :class="bgBlur == true && searchResultShow == false ? 'blur' : ''" v-if="bgObj.type == 'video'" class="bg"  muted autoplay loop :src="bgObj.src"></video>
-      <img :class="bgBlur == true && searchResultShow == false ? 'blur' : ''" v-if="bgObj.type == 'image'" class="bg" :src="bgObj.src"/>
+      <video :class="bgBlur == true ? 'blur' : ''" v-if="bgObj.type == 'video'" class="bg"  muted autoplay loop :src="bgObj.src"></video>
+      <img :class="bgBlur == true ? 'blur' : ''" v-if="bgObj.type == 'image'" class="bg" :src="bgObj.src"/>
     </div>
 
     <!-- 弹窗 -->
@@ -158,19 +167,22 @@ export default {
     return {
       // 背景
       bgObj: {
-        // type: 'video',
-        // src: 'http://hackbinimg.luokangyuan.com/banner/winter1.mp4',
         type: 'image',
         src: ''
         // src: 'http://hackbinimg.luokangyuan.com/searchmore/bg1.jpg'
       },
+      // 输入框聚焦
+      inputFocusBool: false,
       // 控制设置面板显示
       setPanelShow: false,
       // 搜索结果展示
       searchResultShow: false,
       // 输入框下拉关键词
-      searchTipShow: false,
-      searchTipArr: [],
+      searchTipObj: {
+        show: false,
+        data: [],
+        nowIndex: -1,
+      },
       // 背景模糊
       bgBlur: false,
       // 搜索引擎列表
@@ -178,57 +190,75 @@ export default {
         {
           name: "百度",
           browserUrl: 'https://www.baidu.com/s?wd=',
+          resultSrc:'',// 搜索结果的链接
           icon: "icon-baidu",
-          bgColor: "background: #3385ff",
           use: true, // 是否首选项
           active: true, // 是否激活可用状态
           current: true, // 是否为当前选中显示搜索结果页面
+          iconColorStyle:{ // 图标颜色
+            background: "#3385ff",
+          },
+          resultIframeStyle: { //搜索结果的单独样式
+
+          },
         },
         {
           name: "谷歌",
           browserUrl: 'https://google.suanfazu.com/search/?q=',
+          resultSrc:'',// 搜索结果的链接
           icon: "icon-google",
-          bgColor: "background: rgb(251, 188, 5)",
           use: false,
           active: true,
           current: false,
+          iconColorStyle:{
+            background: "rgb(251, 188, 5)",
+          },
+          resultIframeStyle: { //搜索结果的单独样式
+            marginTop: '-53px'
+          },
         },
         {
           name: "360",
           browserUrl: 'https://www.so.com/s?q=',
+          resultSrc:'',// 搜索结果的链接
           icon: "icon-360",
-          bgColor: "background: #1bc550",
           use: false,
           active: true,
           current: false,
+          iconColorStyle:{
+            background: "#1bc550",
+          },
+          resultIframeStyle: {},
         },
         {
           name: "搜狗",
           browserUrl: 'https://www.sogou.com/web?query=',
+          resultSrc:'',
           icon: "icon-sougou",
           bgColor: "background: #f84a19",
           use: false,
           active: true,
           current: false,
+          iconColorStyle:{
+            background: "#f84a19",
+          },
+          resultIframeStyle: {},
         },
         {
           name: "必应",
           browserUrl: 'https://cn.bing.com/search?q=',
+          resultSrc:'',
           icon: "icon-bing",
-          bgColor: "background: #007daa",
           use: false,
           active: true,
           current: false,
-        },
-        {
-          name: "手动添加",
-          browserUrl: '',
-          icon: "icon-add",
-          bgColor: "background: red",
-          use: false,
-          active: true,
-          current: false,
-        },
+          iconColorStyle:{
+            background: "#007daa",
+          },
+          resultIframeStyle: { //搜索结果的单独样式
+            marginTop: '-35px'
+          },
+        }
       ],
       // 搜索输入框值
       searchTxt: '',
@@ -252,126 +282,127 @@ export default {
     // 搜索事件
     searchNow () {
       var searchTxt = this.searchTxt;
-      if (searchTxt != ''){
+      this.$refs.inputbox.blur();
+      if (searchTxt != '') {
         this.searchResultShow = true;
-        this.$refs.onSearch.classList.add("on-search");
-        var resultIframeObjArr = this.$refs.resultPanel;
-        for (var i=0; i<resultIframeObjArr.length; i++) {
-          var thisIfCurrent = resultIframeObjArr[i].getAttribute('current');
-          if(thisIfCurrent == 'true') {
-            // console.log(resultIframeObjArr[i])
-            resultIframeObjArr[i].setAttribute('src', resultIframeObjArr[i].getAttribute("searchUrl") + searchTxt);
+        for(let i=0; i<this.browserArr.length; i++) {
+          if(this.browserArr[i].current == true) {
+            this.browserArr[i].resultSrc = this.browserArr[i].browserUrl + this.searchTxt;
           }
         }
       } else {
         this.alertObj.show = true;
         this.alertObj.text = "请输入搜索内容";
-        this.alertObj.textEn = "no tm empty, ok?";
+        this.alertObj.textEn = "no empty, ojbk?";
       }
     },
 
     // 输入框enter
     inputEnter () {
-      this.searchTipShow = false;
+      this.searchTipObj.show = false;
       this.searchNow();
-      
     },
 
     // 聚焦输入框
     inputFocus () {
-      this.bgBlur = true;
+      // 未展现搜索结果时，改变背景图样式
+      if(this.searchResultShow == false){
+        this.bgBlur = true;
+      }
+      this.inputFocusBool = true;
+      if(this.searchTipObj.data.length > 0) {
+        this.searchTipObj.show = true;
+      }
     },
  
     // 输入框失去焦点
     inputLeave () {
-      this.searchTipShow = false;
+      this.searchTipObj.show = false;
       this.bgBlur = false;
+      if(this.searchTipObj.data.length > 0) {
+        this.inputFocusBool = false;
+      }
     },
 
     // 切换搜索引擎
-    changeBrowser (e) {
-      // 切换按钮激活状态
-      var thisNode = e.currentTarget;
-      var p = thisNode.parentNode.children;
-      var thisBrowserName = thisNode.innerText.trim();
-
-      // 添加激活
-      thisNode.classList.add("active");
-      thisNode.classList.add("hover-bg");
-      // 取消其他激活
-      for ( var i =0,pl= p.length;i<pl;i++) {
-        if (p[i] !== thisNode) {
-          p[i].classList.remove("active");
-          p[i].classList.remove("hover-bg");
+    changeBrowser (index) {
+      for ( var i=0; i<this.browserArr.length; i++) {
+        if (index == i) {
+          this.browserArr[i].current = true;
+        } else {
+          this.browserArr[i].current = false;
         };
       }
-    
-      // 切换显示iframe
-      var thisTxt = thisNode.innerText.trim();
-      // 剔除添加按钮
-      for ( var j = 0; j < this.browserArr.length - 1; j++) {
-        if(this.browserArr[j].name == thisTxt) {
-          this.browserArr[j].current = true;
-        } else {
-          this.browserArr[j].current = false;
-        }
-      }
+      this.searchNow ();
     },
 
+    // 添加搜索引擎
+    addBrowser() {
+      this.alertObj.show = true;
+      this.alertObj.text = "明日开发";
+      this.alertObj.textEn = "too young too simple";
+    },
     // 开启、关闭搜索引擎
-    switchBrowser (e) {
-        var thisTxt = e.currentTarget.innerText.trim();
-        // 剔除添加按钮
-        for ( var k = 0; k < this.browserArr.length - 1; k++) {
-          if(this.browserArr[k].name == thisTxt) {
-            this.browserArr[k].active = !this.browserArr[k].active;
-          }
-        }
+    switchBrowser (index) {
+      this.browserArr[index].active = !this.browserArr[index].active;
     },
 
     // 百度提示
     getBaidu () {
-      this.$http.jsonp('https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su',
-      {
-        params:{wd:this.searchTxt},
-        jsonp:'cb'
-      })
-      .then(function(res){
-        if(res.data.s.length > 0) {
-          this.searchTipArr = res.data.s;
-          this.searchTipShow = true;
-        } else {
-          this.searchTipShow = false;
-        }
-      },function(res){
+      if (this.searchTxt != "") {
+        this.$http.jsonp('https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su',
+        {
+          params:{wd:this.searchTxt},
+          jsonp:'cb'
+        })
+        .then(function(res){
+          if(res.data.s.length > 0) {
+            this.searchTipObj.data = res.data.s;
+            this.searchTipObj.show = true;
+            this.searchTipObj.nowIndex = -1;
+          } else {
+            this.searchTipObj.show = false;
+          }
+        },function(res){
           console.log(res);
-      });
+        });
+      } else {
+        this.searchTipObj.data = [];
+      }
     },
 
     // 选中百度提示
-    selectTip (e) {
-      this.searchTxt = e.currentTarget.innerText.trim();
+    selectTip (index) {
+      this.searchTxt = this.searchTipObj.data[index];
       this.searchNow();
-      this.searchTipShow = false;
+      this.searchTipObj.show = false;
+      this.searchTipObj.nowIndex = index;
     },
 
 
     // 键盘上
     tipUp () {
-      console.log('上')
+      this.searchTipObj.nowIndex--;
+      if(this.searchTipObj.nowIndex <= -1) {
+        this.searchTipObj.nowIndex = this.searchTipObj.data.length - 1;
+      }
+      this.searchTxt = this.searchTipObj.data[this.searchTipObj.nowIndex];
     },
 
     //键盘下
     tipDown () {
-      console.log('下');
-      
+      this.searchTipObj.nowIndex++;
+      if(this.searchTipObj.nowIndex >= this.searchTipObj.data.length) {
+        this.searchTipObj.nowIndex = 0;
+      }
+      this.searchTxt = this.searchTipObj.data[this.searchTipObj.nowIndex];
     },
 
   },
   // 判断移动端
   beforeCreate () {
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-      alert("不想写移动端，百度不好用吗？");
+      alert("检测到你的手机太辣鸡，正在跳转到百度\n请用电脑使用本工具！");
       window.location.href="https://www.baidu.com";
     }
 
@@ -448,13 +479,43 @@ export default {
         z-index: 3;
         width: 640px;
         vertical-align: middle;
+        .line-left{
+          position: absolute;
+          z-index: 100;
+          top:0;
+          left:0;
+          height: 100%;
+          width:1px;
+          background:#de1a1a;
+        }
+        .border-line{
+          position: absolute;
+          z-index: 100;
+          background:@main-color;
+          width:0%;
+          height:1px;
+          transition: @animateTime;
+          &.line-top{
+            left:0;
+            top:0;
+          }
+          &.line-bottom{
+            right:0;
+            bottom:0px;
+            transform: rotate(180deg);
+          }
+          &.focus{
+            width:100%;
+          }
+        }
         .searchmore-input-text{
+          position: relative;
           vertical-align: middle;
           border: none;
           width: 520px;
           height: 40px;
-          border: 1px solid #cfcfcf;
-          border-right:none;
+          border-top:1px solid #f1f1f1;
+          border-bottom:1px solid #f1f1f1;
           background: rgba(255, 255, 255, 0.8);
           line-height: 40px;
           padding: 0 40px 0 10px;
@@ -462,10 +523,6 @@ export default {
           font-size: 16px;
           float: left;
           outline: none;
-          &:focus{
-            // box-shadow: 0px 0px 20px #fdeaea inset;
-            border-color: rgb(247, 74, 74);
-          }
         }
         .searchmore-submit{
           display: block;
@@ -524,7 +581,7 @@ export default {
               justify-content:center;
               width: 100px;
               height: 100px;
-              // border:1px solid #000;
+              user-select: none;
               cursor: pointer;
               float: left;
               .browser-icon{
@@ -593,7 +650,7 @@ export default {
         margin-top: 40px;
         background: rgba(255, 255, 255, 0.8);
         box-shadow: @box-shadow;
-        li{
+        .tip-item{
           list-style: none;
           padding: 0 10px;
           height: 30px;
@@ -605,10 +662,13 @@ export default {
             transition: @animateTime;
           }
           &:hover{
-            span{
-              transform: translateX(5px);
-            }
-            background-color: #ffffff;
+            border-left: 1px solid @main-color;
+            background-color: #f1f1f1;
+            color: @main-color;
+          }
+          &.active{
+            border-left: 1px solid @main-color;
+            background-color: #f1f1f1;
             color: @main-color;
           }
         }
@@ -706,11 +766,11 @@ export default {
     align-items:center;
     width: 100%;
     height: 100%;
-    background: #f1f1f1;
+    background: #ffffff;
     .bg{
       transition: @animateTime;
       &.blur{
-        filter: blur(2px) brightness(80%);
+        filter: brightness(70%);
       }
       width: 102%;
     }
